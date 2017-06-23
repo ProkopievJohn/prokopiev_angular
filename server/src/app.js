cccd4b18a988cce1d62ac96e65dd41055d1507d9
Prokopiev Ivan <ProkopievJohn@gmail.com>
2017-06-23 21:06:11 +0300

    add cluster with errors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,21 @@
 import Koa from 'koa'
 import middlewares from './middlewares'
+import config from './configs/server-config'
+import mongoose from 'mongoose'
 
 const app = new Koa();
 
 middlewares(app);
 
-export default app;
+mongoose.connect(config.getDatabaseHost());
+
+process.env.NODE_ENV !== 'test' && mongoose.connection.on('connected', () => {
+    console.log('Mongoose default connection open to ' + config.getDatabaseHost());
+    app.listen(config.getPort(), () => {
+        console.log('app start at ==>> ', config.getHost() + ':' + config.getPort());
+    });
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose default connection error: ', err);
+});
--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,60 @@
-import config from './configs/server-config'
-import mongoose from 'mongoose'
-import app from './app'
+require('babel-register');
 
-mongoose.connect(config.getDatabaseHost());
+const cluster = require('cluster');
 
-process.env.NODE_ENV !== 'test' && mongoose.connection.on('connected', () => {
-    console.log('Mongoose default connection open to ' + config.getDatabaseHost());
-    app.listen(config.getPort(), () => {
-        console.log('app start at ==>> ', config.getHost() + ':' + config.getPort());
+if (cluster.isMaster) {
+    const cpus = require('os').cpus().length * 2;
+    for (let i = 0; i < cpus; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log('[WARN] Worker %d died with code/signal %s. Restarting worker...', worker, code, signal);
+        cluster.fork();
     });
-});
+} else {
+    require('./app')
+}
 
-mongoose.connection.on('error', (err) => {
-    console.log('Mongoose default connection error: ', err);
-});
+
+// const cluster = require('cluster')
+//   , numCPUs = require('os').cpus().length
+//   , bodyParser = require('koa-body-parser')
+//   , cors = require('koa-cors')
+//   , koa = require('koa')
+//   , router = require('koa-router')()
+//   , controller = require('./controller.js')
+//   , app = module.exports = koa();
+
+// if (cluster.isMaster) {
+//   // Fork workers
+//   console.log(`Detected ${numCPUs} CPUs, forking...`);
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+// } else {
+
+//   app
+//     .use(cors())
+//     .use(bodyParser())
+//     .use(router.routes())
+//     .use(router.allowedMethods());
+
+//   router
+//     .get('/health', function *() {
+//       this.body = 'up';
+//       this.status = 200;
+//     })
+//     .post('/application', function *() {
+//       try {
+//         this.body = 'Success';
+//         this.status = 200;
+//       } catch (err) {
+//         this.body = {"Error": err};
+//         this.status = 400;
+//       }
+//     });
+
+//   app.listen(8000);
+// }
